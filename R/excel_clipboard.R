f2c8d5b6aa7cc1003fe17f3a3dcde1acee177bec
Rodrigo Revilla <rorevillaca@gmail.com>
2021-04-03 23:22:11 -0600

    Added copy_to_excel function

--- R/excel_clipboard.R
+++ R/excel_clipboard.R
@@ -1,4 +1,4 @@
-#' Paste from excel (clipboard)
+#' Paste from Excel (via clipboard)
 #'
 #' Imports a copied Excel region into a dataframe
 #'
@@ -11,4 +11,21 @@
 #' @importFrom utils read.table
 paste_from_excel <- function(header=TRUE,...) {
   read.table("clipboard",sep="\t",header=header,...)
+}
+
+
+
+#' Copy to Excel (via clipboard)
+#'
+#' @param x dataframe to be copied to Excel
+#' @param row.names Binary - Indicates whether the row names should be copied
+#' @param col.names Binary - Indicates whether the column names should be copied
+#' @param ... params used in write.table function (from utils package)
+#'
+#' @return None
+#' @export
+#'
+#' @importFrom utils write.table
+copy_to_excel <- function(x,row.names=FALSE,col.names=TRUE,...) {
+  write.table(x,"clipboard",sep="\t",row.names=row.names,col.names=col.names,...)
 }
