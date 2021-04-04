63c155e93a9390c205fb9d3606e1e1cc518f0e66
Rodrigo Revilla <rorevillaca@gmail.com>
2021-04-03 23:27:45 -0600

    Added palettes

---
+++ R/palettes.R
@@ -0,0 +1,37 @@
+#' Credits to Jake Kaupp (jkaupp) for compiling these palettes
+#' Qualitative color schemes by Paul Tol
+#' @export
+tol1qualitative <- c("#4477AA")
+#' Qualitative color schemes by Paul Tol
+#' @export
+tol2qualitative <- c("#4477AA", "#CC6677")
+#' Qualitative color schemes by Paul Tol
+#' @export
+tol3qualitative <- c("#4477AA", "#DDCC77", "#CC6677")
+#' Qualitative color schemes by Paul Tol
+#' @export
+tol4qualitative <- c("#4477AA", "#117733", "#DDCC77", "#CC6677")
+#' Qualitative color schemes by Paul Tol
+#' @export
+tol5qualitative <- c("#332288", "#88CCEE", "#117733", "#DDCC77", "#CC6677")
+#' Qualitative color schemes by Paul Tol
+#' @export
+tol6qualitative <- c("#332288", "#88CCEE", "#117733", "#DDCC77", "#CC6677","#AA4499")
+#' Qualitative color schemes by Paul Tol
+#' @export
+tol7qualitative <- c("#332288", "#88CCEE", "#44AA99", "#117733", "#DDCC77", "#CC6677","#AA4499")
+#' Qualitative color schemes by Paul Tol
+#' @export
+tol8qualitative <- c("#332288", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#CC6677","#AA4499")
+#' Qualitative color schemes by Paul Tol
+#' @export
+tol9qualitative <- c("#332288", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#CC6677", "#882255", "#AA4499")
+#' Qualitative color schemes by Paul Tol
+#' @export
+tol10qualitative <- c("#332288", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#661100", "#CC6677", "#882255", "#AA4499")
+#' Qualitative color schemes by Paul Tol
+#' @export
+tol11qualitative <- c("#332288", "#6699CC", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#661100", "#CC6677", "#882255", "#AA4499")
+#' Qualitative color schemes by Paul Tol
+#' @export
+tol12qualitative <- c("#332288", "#6699CC", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#661100", "#CC6677", "#AA4466", "#882255", "#AA4499")
